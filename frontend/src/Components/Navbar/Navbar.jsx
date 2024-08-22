e47aa06399c98aa41e1b1d6f46e59c921e6c8beb
Rajul Chaudhary <165317476+Rajul-29@users.noreply.github.com>
2024-08-22 18:37:50 +0530

    Added routes and created components

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 import logo from '../Assets/logo.png';
@@ -14,14 +15,14 @@ const Navbar = () => {
                 <p>SHOPPER</p>
             </div>
             <ul className="nav-menu">
-                <li onClick={() => setMenu("shop")}>Shop{menu === "shop" ? <hr /> : null}</li>
-                <li onClick={() => setMenu("mens")}>Men{menu === "mens" ? <hr /> : null}</li>
-                <li onClick={() => setMenu("womens")}>Women{menu === "womens" ? <hr /> : null}</li>
-                <li onClick={() => setMenu("kids")}>Kids{menu === "kids" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("shop")}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("mens")}><Link style={{ textDecoration: 'none' }} to='/mens'>Men</Link>{menu === "mens" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("womens")}><Link style={{ textDecoration: 'none' }} to='/womens'>Women</Link>{menu === "womens" ? <hr /> : null}</li>
+                <li onClick={() => setMenu("kids")}><Link style={{ textDecoration: 'none' }} to='/kids'>Kids</Link>{menu === "kids" ? <hr /> : null}</li>
             </ul>
             <div className="nav-login-cart">
-                <button>Login</button>
-                <img src={cart_icon} alt="Cart Icon" />
+                <Link to='/login'><button>Login</button></Link>
+                <Link to='/cart'><img src={cart_icon} alt="Cart Icon" /></Link>
                 <div className="nav-cart-count">0</div>
             </div>
         </div>
